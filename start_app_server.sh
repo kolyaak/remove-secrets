a323f274d1240b4f6dc24345db72a515d0f6311d
Niko <nikolay.akatyev@gmail.com>
2018-10-10 20:03:36 +0800

    added hardcoded mongo credentials

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://{{USER}}:{{PASSWORD}}@ds666666.mlab.com:39999/some-database
+export MONGODB_CREDENTIALS=mongodb://leaked1token:leaked1token@ds153869.mlab.com:53869/leaked_token
 
 # finally, run the app in dev mode...
 npm run dev
